b0d4c15e45b0ddaddb998737530dca1bdc32772b
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-11-16 09:36:52 +0300

    Create app's base style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Hello</h1>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app> </v-navigation-drawer>
+
+    <v-app-bar app dark color="blue darken-4">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text large elevation="2">Link</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({ drawer: false }),
+};
 </script>
 
 <style>
