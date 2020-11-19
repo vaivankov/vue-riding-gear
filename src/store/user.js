37b75884547a20d3249976c1b10f6302da5447fd
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-11-19 08:13:18 +0300

    Add registration state

--- src/store/user.js
+++ src/store/user.js
@@ -16,13 +16,19 @@ export default {
     }
   },
   actions: {
-    registerUser({ commit }, { email, password }) {
-      firebase
-        .default.auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then((user) => {
-          commit('setUser', new User(user.user.uid))
-        })
+    async registerUser({ commit }, { email, password }) {
+      commit('clearError')
+      commit('setLoading', true)
+      try {
+        const user = await firebase.default.auth()
+          .createUserWithEmailAndPassword(email, password)
+        commit('setUser', new User(user.user.uid))
+        commit('setLoading', false)
+      } catch (err) {
+        commit('setLoading', false)
+        commit('setError', err.message)
+        throw err
+      }
     }
   },
   getters: {
